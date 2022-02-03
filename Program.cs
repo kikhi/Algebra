fe08d6dc50368d3cb8a37356d665348d9f2e094b
Cesar Trujillo <catg20g@gmail.com>
2022-02-02 22:21:28 -0800

    Algebra sets

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Algebra
+{
+    class Sets
+    {
+        
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("========  Algebra calculator  ========");
+                
+
+            Console.ReadKey();
+        }
+    }
+}
