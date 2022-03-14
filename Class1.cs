7fc7e8283a2a88b28d7c12f71c0bc1944afdfd4f
Cesar Trujillo <catg20g@gmail.com>
2022-03-14 14:25:04 -0700

    Algebra

--- Class1.cs
+++ Class1.cs
@@ -1,5 +1,28 @@
 ﻿namespace Algebra;
-public class Class1
+public class FormulasAlgebra
 {
+    // ( a + b )^2 = a^2 + b^2 + 2ab
+    private static int Formula1(int a, int b)
+    {
+        return ((a)^2) + ((b)^2) + (2*(a)*(b));
+    }
+
+    // ( a - b )^2 = a^2 + b^2 - 2ab
+    private static int Formula2(int a, int b)
+    {
+        return ((a)^2) + ((b)^2) - (2*(a)*(b));
+    }
+
+    // a^2 - b^2 = (a + b) - (a - b)
+    private static int Formula3(int a, int b)
+    {
+        return (a + b) - (a - b);
+    }
+    
+    // a^2 + b^2 = (a + b)^2 - 2ab
+    private static int Formula4(int a, int b)
+    {
+        return ((a + b)^2) + (2*(a)*(b));
+    }
 
 }
