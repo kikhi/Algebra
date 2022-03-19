5c336289d4697333e261166557a60d63af997fcb
Cesar Trujillo <catg20g@gmail.com>
2022-03-18 20:37:23 -0700

    Algebra library

--- Class1.cs
+++ Class1.cs
@@ -25,4 +25,22 @@ public class FormulasAlgebra
         return ((a + b)^2) + (2*(a)*(b));
     }
 
+    // (a + b)^3 = a^3 + 3a^2b + 3ab^2 + b^3
+    private static int Formula5(int a, int b)
+    {
+        return ((a)^3) + 3*((a)^2)*(b) + 3*(a)*((b)^2) + ((b)^3);
+    }
+
+    // (a - b)^3 = a^3 - 3a^2b + 3ab^2 - b^3
+    public static int Formula6(int a, int b)
+    {
+        return ((a)^3) - 3*((a)^2)*(b) + 3*(a)*((b)^2) - ((b)^3);
+    }
+
+    // a^3 + b^3 = (a + b) (a^2 - ab + b^2) 
+    public static int Formula7(int a, int b)
+    {
+        return (a + b) * (a^2 - (a * b) + b^2);
+    }
+
 }
