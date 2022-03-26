0527bb2c23a7ebc7b3077f47c7072b5e56eb47b0
Cesar Trujillo <catg20g@gmail.com>
2022-03-26 13:56:34 -0700

    Algebra add testing passed

--- AlgebraTests/UnitTest1.cs
+++ AlgebraTests/UnitTest1.cs
@@ -1,12 +1,62 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace AlgebraTests;
 
-[TestClass]
 public class UnitTest1
 {
-    [TestMethod]
-    public void TestMethod1()
+    [Fact]
+    // ( a + b )^2
+    public void TestFormula1()
     {
+        double result = Algebra.FormulasAlgebra.Formula1(5, 10);
+        Assert.Equal(225, result);
+    }
+
+    [Fact]
+    // ( a - b )^2
+    public void TestFormula2()
+    {
+        double result = Algebra.FormulasAlgebra.Formula2(5, 10);
+        Assert.Equal(25, result);
+    }
+    
+    [Fact]
+    // a^2 - b^2
+    public void TestFormula3()
+    {
+        double result = Algebra.FormulasAlgebra.Formula3(5, 10);
+        Assert.Equal(-75, result);
+    }
+
+    [Fact]
+    // a^2 + b^2
+    public void TestFormula4()
+    {
+        double result = Algebra.FormulasAlgebra.Formula4(5, 10);
+        Assert.Equal(125, result);
+    }
+
+    [Fact]
+    // (a + b)^3
+    public void TestFormula5()
+    {
+        double result = Algebra.FormulasAlgebra.Formula5(5, 10);
+        Assert.Equal(3375, result);
+    }
+    
+    [Fact]
+    // (a - b)^3
+    public void TestFormula6()
+    {
+        double result = Algebra.FormulasAlgebra.Formula6(5, 10);
+        Assert.Equal(-125, result);
+    }
+
+    [Fact]
+    // a^3 + b^3
+    public void TestFormula7()
+    {
+        double result = Algebra.FormulasAlgebra.Formula7(5, 10);
+        Assert.Equal(1125, result);
     }
 }
--- Class1.cs
+++ Class1.cs
@@ -2,45 +2,45 @@
 public class FormulasAlgebra
 {
     // ( a + b )^2 = a^2 + b^2 + 2ab
-    public static int Formula1(int a, int b)
+    public static double Formula1(int a, int b)
     {
-        return ((a)^2) + ((b)^2) + (2*(a)*(b));
+        return ( Math.Pow(a,2) ) + ( Math.Pow(b,2) ) + ( 2 * a * b );
     }
 
     // ( a - b )^2 = a^2 + b^2 - 2ab
-    public static int Formula2(int a, int b)
+    public static double Formula2(int a, int b)
     {
-        return ((a)^2) + ((b)^2) - (2*(a)*(b));
+        return ( Math.Pow(a,2) ) + ( Math.Pow(b,2) ) - ( 2 * a * b );
     }
 
-    // a^2 - b^2 = (a + b) - (a - b)
+    // a^2 - b^2 = (a + b) * (a - b)
     public static int Formula3(int a, int b)
     {
-        return (a + b) - (a - b);
+        return ( a + b ) * ( a - b );
     }
     
     // a^2 + b^2 = (a + b)^2 - 2ab
-    public static int Formula4(int a, int b)
+    public static double Formula4(int a, int b)
     {
-        return ((a + b)^2) + (2*(a)*(b));
+        return ( Math.Pow(a + b, 2 ) ) - ( 2 * a * b );
     }
 
     // (a + b)^3 = a^3 + 3a^2b + 3ab^2 + b^3
-    public static int Formula5(int a, int b)
+    public static double Formula5(int a, int b)
     {
-        return ((a)^3) + 3*((a)^2)*(b) + 3*(a)*((b)^2) + ((b)^3);
+        return (Math.Pow(a,3)) + (3*( Math.Pow(a,2) )*(b)) + 3*(a)*( Math.Pow(b,2) ) + (Math.Pow(b,3));
     }
 
     // (a - b)^3 = a^3 - 3a^2b + 3ab^2 - b^3
-    public static int Formula6(int a, int b)
+    public static double Formula6(int a, int b)
     {
-        return ((a)^3) - 3*((a)^2)*(b) + 3*(a)*((b)^2) - ((b)^3);
+        return (Math.Pow(a,3)) - (3 * Math.Pow(a,2) * (b)) + (3 * (a) * Math.Pow(b,2)) - (Math.Pow(b,3));
     }
 
     // a^3 + b^3 = (a + b) (a^2 - ab + b^2) 
-    public static int Formula7(int a, int b)
+    public static double Formula7(int a, int b)
     {
-        return (a + b) * (a^2 - (a * b) + b^2);
+        return (a + b) * ((Math.Pow(a,2)) - (a * b) + (Math.Pow(b,2)));
     }
 
 }
